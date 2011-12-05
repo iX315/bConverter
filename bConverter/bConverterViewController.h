9cad4d3a5e64f2d31d80aa20035732a97481b2b6
iX315 <x3vius.315@gmail.com>
2011-12-05 16:58:51 +0100

    v0.2

--- bConverter/bConverterViewController.h
+++ bConverter/bConverterViewController.h
@@ -9,5 +9,28 @@
 #import <UIKit/UIKit.h>
 
 @interface bConverterViewController : UIViewController
+{
+    UITextField *input;
+    UILabel *output;
+    UIImageView *glow;
+    UIImageView *troll;
+    UIImage *dec_up;
+    UIImage *mask_up;
+    UIScrollView *inputScroll;
+    UIScrollView *outputScroll;
+    UISwipeGestureRecognizer *drecognizer;
+    UISwipeGestureRecognizer *urecognizer;
+}
 
+@property (nonatomic, retain) IBOutlet UITextField *input;
+@property (nonatomic, retain) IBOutlet UILabel *output;
+@property (nonatomic, retain) IBOutlet UIImageView *glow;
+@property (nonatomic, retain) IBOutlet UIImageView *troll;
+@property (nonatomic, retain) IBOutlet UIImage *dec_up;
+@property (nonatomic, retain) IBOutlet UIImage *mask_up;
+@property (nonatomic, retain) IBOutlet UIScrollView *inputScroll;
+@property (nonatomic, retain) IBOutlet UIScrollView *outputScroll;
+
+-(IBAction)endEdit;
+-(IBAction)initEdit;
 @end
--- bConverter/bConverterViewController.m
+++ bConverter/bConverterViewController.m
@@ -7,8 +7,18 @@
 //
 
 #import "bConverterViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @implementation bConverterViewController
+
+@synthesize input;
+@synthesize output;
+@synthesize glow;
+@synthesize troll;
+@synthesize mask_up;
+@synthesize dec_up;
+@synthesize inputScroll;
+@synthesize outputScroll;
 
 - (void)didReceiveMemoryWarning
 {
@@ -20,9 +30,120 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+    inputScroll.layer.mask = YES;
+    
+    /*
+    UIView *mask = [[CustomMask alloc] init];
+    [_view layer].mask =[mask layer];
+    */
+    drecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeDown:)];
+    [drecognizer setDirection:(UISwipeGestureRecognizerDirectionDown)];
+    [[self view] addGestureRecognizer:drecognizer];
+    urecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeUp:)];
+    [urecognizer setDirection:(UISwipeGestureRecognizerDirectionUp)];
+    [[self view] addGestureRecognizer:urecognizer];
+    
 	// Do any additional setup after loading the view, typically from a nib.
+    [super viewDidLoad];
 }
+
+-(IBAction)endEdit {
+    [input resignFirstResponder];
+    troll.alpha = 0.0;
+    NSString *inputValue = [input text];
+    //snippets converters here
+    if ([inputValue isEqualToString:@"artofapps"]) {
+        [output setText:@"credits"];
+    }else if ([inputValue isEqualToString:@"troll"]) {
+        troll.alpha = 1.0;
+        [output setText:nil];
+    } else {
+        NSMutableString *str = [[NSMutableString alloc] initWithString:@""];
+        if(inputValue.integerValue > 0){
+            for(NSInteger numberCopy = inputValue.integerValue; numberCopy > 0; numberCopy >>= 1){	
+                // Prepend "0" or "1", depending on the bit
+                [str insertString:((numberCopy & 1) ? @"1" : @"0") atIndex:0];
+            }
+        }
+        else if(inputValue.integerValue == 0){
+            [str insertString:@"0" atIndex:0];
+        }
+        [output setText:str];
+    }
+    [[self view] addGestureRecognizer:drecognizer];
+    [[self view] addGestureRecognizer:urecognizer];
+}
+
+-(IBAction)initEdit {
+    [[self view] removeGestureRecognizer:urecognizer];
+    [[self view] removeGestureRecognizer:drecognizer];
+}
+
+#pragma mark - Animations
+
+-(void)handleSwipeDown:(UISwipeGestureRecognizerDirection *)sender {
+    //NSLog(@"Swipe received.");
+    
+    [NSTimer scheduledTimerWithTimeInterval:3.0 target:self selector:@selector(animation_end:) userInfo:nil repeats:NO];
+
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:1.0];
+    input.transform = CGAffineTransformMakeTranslation(0.0,140.0);        
+    [NSTimer scheduledTimerWithTimeInterval:0.6 target:self selector:@selector(alpha_on:) userInfo:nil repeats:NO];
+    [NSTimer scheduledTimerWithTimeInterval:1.2 target:self selector:@selector(alpha_back:) userInfo:nil repeats:NO];
+    [UIView commitAnimations];
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:3.0];
+    [[self view] removeGestureRecognizer:urecognizer];
+    [[self view] removeGestureRecognizer:drecognizer];
+    output.transform = CGAffineTransformMakeTranslation(0.0,140.0);
+    troll.transform = CGAffineTransformMakeTranslation(0.0,140.0);
+    [UIView commitAnimations];
+}
+
+-(void)handleSwipeUp:(UISwipeGestureRecognizerDirection *)sender {
+     //NSLog(@"Swipe received.");
+    [NSTimer scheduledTimerWithTimeInterval:2.0 target:self selector:@selector(animation_end:) userInfo:nil repeats:NO];
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:1.0];
+    output.transform = CGAffineTransformMakeTranslation(0.0,0.0);
+    troll.transform = CGAffineTransformMakeTranslation(0.0,0.0);
+    [UIView commitAnimations];
+            
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:2.0];
+    [[self view] removeGestureRecognizer:urecognizer];
+    [[self view] removeGestureRecognizer:drecognizer];
+    input.transform = CGAffineTransformMakeTranslation(0.0,0.0);
+    [UIView commitAnimations];
+}
+
+
+- (void)alpha_on:(NSTimer *)thetime {
+	[UIView beginAnimations:nil context:NULL];
+	[UIView setAnimationDuration:0.5];
+    glow.alpha = 1.0;
+	[UIView commitAnimations];
+}
+
+- (void)alpha_back:(NSTimer *)thetime {
+	[UIView beginAnimations:nil context:NULL];
+	[UIView setAnimationDuration:0.5];
+    glow.alpha = 0.0;
+	[UIView commitAnimations];
+}
+
+- (void)animation_end:(NSTimer *)thetime {
+    [[self view] addGestureRecognizer:drecognizer];
+    [[self view] addGestureRecognizer:urecognizer];
+}
+
+#pragma mark - UI
+
+
+#pragma mark - Others
 
 - (void)viewDidUnload
 {
